b5c378ba345a28de23ff87769261ed73895cac82
FullStackFranklin <107124830+FullStackFranklin@users.noreply.github.com>
2022-09-12 14:55:43 -0400

    Add files via upload

---
+++ madlib.js
@@ -0,0 +1,15 @@
+var enterAName = prompt("Enter any name: ");
+var enterADestination = prompt("Enter any destination")
+var jobTitle = prompt("Enter a job title: ");
+var enterNumber = prompt("Enter a number betweeen 1 and 100000: ");
+var enterAResponseToNewJob = prompt("Enter a response: ");
+var verb = prompt("Enter a verb not ending in I-N-G: ");
+var adjective = prompt("Enter a adjective :");
+var pluralNoun = prompt("Enter a plural noun : ");
+
+
+
+
+document.write(`${enterAName} just told me that he/she was just offered the opportunity to relocate to  ${enterADestination} to work full-time as a  ${jobTitle} making  ${enterNumber} a year. I was so shocked I said ${enterAResponseToNewJob}  your job requires you to  ${verb} and make ${adjective} ${pluralNoun}.`);
+
+ 
